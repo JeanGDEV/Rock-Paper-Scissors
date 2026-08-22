d7efadce769a8c94e2b8f09a3641e5a3c690214c
JeanGDEV <123316415+JeanGDEV@users.noreply.github.com>
2026-08-21 21:46:21 -0400

    Fix logic error addEventListener

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let humanScore = 0
 let computerScore = 0
 
 // Buttons
-const rockButton = document.querySelector("#rock-btn")
-const paperButton = document.querySelector("#paper-btn")
-const scissorsButton = document.querySelector("#scissors-btn")
+const rockButton = document.querySelector(".rock-btn")
+const paperButton = document.querySelector(".paper-btn")
+const scissorsButton = document.querySelector(".scissors-btn")
 
-rockButton.addEventListener("click", playRound("rock", getComputerChoice()))
-paperButton.addEventListener("click", playRound("paper", getComputerChoice()))
-scissorsButton.addEventListener("click", playRound("scissors", getComputerChoice()))
+rockButton.addEventListener("click",() => playRound("rock", getComputerChoice()))
+paperButton.addEventListener("click",() => playRound("paper", getComputerChoice()))
+scissorsButton.addEventListener("click",() => playRound("scissors", getComputerChoice()))
 
 
 // Choices Section
@@ -32,39 +32,39 @@ function playRound(humanChoice, computerChoice) {
     
     // Rock Section
     if (humanChoice == "rock" && computerChoice == "rock") {
-        alert("Draw!")
+        console.log("Draw!")
 
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         computerScore++
-        alert("You Lose!")
+        console.log("You Lose!")
 
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
         humanScore++
-        alert("You Win!")
+        console.log("You Win!")
 
     // Paper Section
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         humanScore++
-        alert("You Win!")
+        console.log("You Win!")
 
     } else if (humanChoice == "paper" && computerChoice == "paper") {
-        alert("Draw!")
+        console.log("Draw!")
 
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         computerScore++
-        alert("You Lose!")
+        console.log("You Lose!")
 
     // Scissors Section
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         computerScore++
-        alert("You Lose!")
+        console.log("You Lose!")
 
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         humanScore++
-        alert("You Win!")
+        console.log("You Win!")
 
     } else if (humanChoice == "scissors" && computerChoice == "scissors") {
-        alert("Draw!")
+        console.log("Draw!")
     }
 
 }
@@ -87,5 +87,5 @@ function playGame() {
         Machine: ${computerScore}`)
 }
 
-playGame()
+
 
